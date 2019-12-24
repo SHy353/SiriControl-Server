360b1a3574ad0419d3bfe44dd5ec0473fedea271
Sanjeet <21122782+thesanjeetc@users.noreply.github.com>
2019-12-24 11:17:07 +0000

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,10 @@ import socketio
 
 sio = socketio.Client()
 
-# Replace with your generated token
+# Replace with your generated token.
 token = "a598fbce-7cc7-4240-ad35-0b86b2651c59"
+
+# Replace siricontrol-server with the name of your app.
 sio.connect('https://siricontrol-server.herokuapp.com/?token=' + token)
 
 
